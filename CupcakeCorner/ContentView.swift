60fb33852c7c7d5f3b9711ce944684413cd44526
Joel Lacerda <joellacerdaol@gmail.com>
2024-11-21 20:11:22 -0300

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Joel Lacerda on 21/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
