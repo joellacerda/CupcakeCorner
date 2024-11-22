db245119b08244f3f600a353a5739438723e8dbb
Joel Lacerda <joellacerdaol@gmail.com>
2024-11-22 20:32:48 -0300

    Take basic order details

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Joel Lacerda on 22/11/24.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    var extraFrosting = false
+    var addSprinkles = false
+}
